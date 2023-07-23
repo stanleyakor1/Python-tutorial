341a4c703a70a97361bb969570ededd60d0057d2
stanleyakor1 <stanleyakor@u.boisestate.edu>
2023-07-22 22:38:28 -0600

    update

--- wrf-python/pysubset.py
+++ wrf-python/pysubset.py
@@ -25,7 +25,7 @@ if __name__ == '__main__':
 	path = '/bsuhome/stanleyakor/scratch/'+ scheme +'/' 
 
 	home_dir = os.path.expanduser("~")
-	output_dir = os.path.join(home_dir,'DJF',scheme,'/')
+	output_dir = os.path.join(home_dir,'DJF',scheme)
 
 	files =  sorted(glob.glob(path + 'wrfout_d02_2022-12-01*'))
 	d = xr.open_mfdataset(files, concat_dim='Time',combine='nested')
@@ -64,6 +64,6 @@ if __name__ == '__main__':
 	        
 		 	print(f"Error creating the directory: {e}")
 	
-	new_array.to_netcdf(output_dir + scheme + '_DJF'+".nc")
+	new_array.to_netcdf(output_dir +'/'+ scheme + '_DJF'+".nc")
 	
 	del d, new_array
